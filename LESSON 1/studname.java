8970f275811f921228ebdf9198e951017aa0a947
Mariel A. Monte de Ramos <111344331+mariel-ramos@users.noreply.github.com>
2023-04-14 16:46:08 +0800

    Create studname.java

---
+++ LESSON 1/studname.java
@@ -0,0 +1,27 @@
+public class StudName {
+       private String studNumber;
+       private String studName;
+       private String studCourse;
+       private String studYear;
+
+       public StudName(String studNumber, String studName, String studCourse, String studYear){
+        this.studNumber = studNumber;
+        this.studName = studName;
+        this.studCourse = studCourse;
+        this.studYear = studYear;
+     }
+
+        public void displayStudInfo() {
+          System.out.println("studNumber:" + this.studNumber);
+          System.out.println("studName:" + this.studName);
+          System.out.println("studCourse:" + this.studCourse);
+          System.out.println("studYear:" + this.studYear);
+        }
+
+        //example data:
+
+        public static void public static void main(String[] args) {
+          StudName name = new sun.
+          
+        }
+}
